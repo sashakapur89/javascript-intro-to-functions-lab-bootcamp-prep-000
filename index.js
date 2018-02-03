4c8fcbf9a6f1ce5298c2f89b50b7fe8bc0789a01
Sasha Kapur <sashakapur89@gmail.com>
2018-02-03 03:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ function sayHiToGrandma(string){
     return `None of the above.`;
   }
 }
-function say(greeting, firstName) {
-  return `${greeting}, ${firstName}!`
+
+function checkData() {
+  if (document.form1.threeChar.value.length == 3) {
+    return true;
+  } else {
+    alert('Enter exactly three characters. ' +
+    document.form1.threeChar.value + ' is not valid.');
+    return false;
+  }
 }
