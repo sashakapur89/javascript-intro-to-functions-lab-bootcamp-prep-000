a4f40b3f8f8d21c239f3dceba5a251361ae5cd9f
Sasha Kapur <sashakapur89@gmail.com>
2018-02-03 03:12:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function logWhisper(string){console.log(string.toLowerCase())}
 
 function sayHiToGrandma(string){
     if (string === string.toLowerCase()){
-    return `I can't hear you!`;
+    return "I can't hear you!";
   } else if (string.toUpperCase() === string){
-    return `YES INDEED!`;
+    return 'YES INDEED!';
   } else if (string === "I love you, Grandma."){
     return `I love you, too.`;
   } else {
