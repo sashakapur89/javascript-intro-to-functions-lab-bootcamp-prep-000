2f9217ce8621803df3881294fc85f0495cf7e2d7
Sasha Kapur <sashakapur89@gmail.com>
2018-02-03 03:11:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function shout(string){return string.toUpperCase()}
 
 function whisper(string){return string.toLowerCase()}
 
-function logShout(string){console.log(`hello`.toUpperCase())}
+function logShout(string){console.log(string.toUpperCase())}
 
 function logWhisper(string){console.log(`hello`.toLowerCase())}
 
