145693ca1123d3f82ca7024dbad85498bc53a9df
Sasha Kapur <sashakapur89@gmail.com>
2018-02-03 03:12:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function whisper(string){return string.toLowerCase()}
 
 function logShout(string){console.log(string.toUpperCase())}
 
-function logWhisper(string){console.log(`hello`.toLowerCase())}
+function logWhisper(string){console.log(string.toLowerCase())}
 
 function sayHiToGrandma(string){
     if (string === string.toLowerCase()){
